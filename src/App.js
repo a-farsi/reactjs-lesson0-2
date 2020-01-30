b75d4c4f2944dbf7b830c7c6e43ea57e82f5b3b9
a-farsi <abdelhak.farsi@gmail.com>
2020-01-30 17:11:18 +0100

    use of list and key id

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Karouer", age: 24 },
+      { name: "Pjhsyue", age: 25 },
+      { name: "Xwiayr", age: 29 }
+    ],
+    showPersons: false
+  };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    });
+  };
+
+  deletePersonHandler(personIndex) {
+    const newPersons = [...this.state.persons];
+    newPersons.splice(personIndex, 1);
+    this.setState({ persons: newPersons });
+  }
+
+  render() {
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((pers, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={pers.name}
+                age={pers.age}
+                key={index}
+              ></Person>
+            );
+          })}
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>How to use lists in react</h1>
+        <button onClick={this.togglePersonHandler}>toggle persons</button>
+        {persons}
+      </div>
+    );
+  }
 }
 
 export default App;

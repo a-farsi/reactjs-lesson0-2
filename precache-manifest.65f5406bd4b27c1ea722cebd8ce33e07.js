da27b0199e244c517ce7bc3013f9e2b291f3843c
a-farsi <abdelhak.farsi@gmail.com>
2020-01-30 17:17:43 +0100

    Updates

---
+++ precache-manifest.65f5406bd4b27c1ea722cebd8ce33e07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c7d0d622002c52ff8252d26a7956aa",
+    "url": "/reactjs-lesson0-2/index.html"
+  },
+  {
+    "revision": "550326359158f0342cbf",
+    "url": "/reactjs-lesson0-2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f597fcf7e2530b5bd63d",
+    "url": "/reactjs-lesson0-2/static/js/2.9f72b404.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactjs-lesson0-2/static/js/2.9f72b404.chunk.js.LICENSE"
+  },
+  {
+    "revision": "550326359158f0342cbf",
+    "url": "/reactjs-lesson0-2/static/js/main.2d1c6b7e.chunk.js"
+  },
+  {
+    "revision": "9e512693b6995a5397d9",
+    "url": "/reactjs-lesson0-2/static/js/runtime-main.a4896ec7.js"
+  }
+]);
